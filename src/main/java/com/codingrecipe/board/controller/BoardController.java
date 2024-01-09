3f6ef79b32d8c0fca41885fb22d57d0750348956
mh4111 <bogyeom1@naver.com>
2024-01-10 00:24:13 +0900

    springframework board 글작성 구현

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,30 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        int saveResult = boardService.save(boardDTO);
+        if(saveResult > 0){
+            return "redirect:/board/";
+        }else {
+            return "save";
+        }
+    }
+}
---
+++ src/main/java/com/codingrecipe/board/service/BoardService.java
@@ -0,0 +1,16 @@
+package com.codingrecipe.board.service;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public int save(BoardDTO boardDTO) {
+        return boardRepository.save(boardDTO);
+    }
+}
